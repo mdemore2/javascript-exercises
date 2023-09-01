d80084e9190215c0eea3c8590083bb8edbfcdddb
Mark Demore <53943861+mdemore2@users.noreply.github.com>
2023-09-01 15:08:31 -0400

    Completed exercise 04
    Completed removeFromArray function
    Added check for val in array
    Allowed for multiple items to be passed to func

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(originalArray, ...elementsToRemove) {
+    for (element of elementsToRemove){
+        if (originalArray.includes(element)){
+            let indexToRemove = originalArray.indexOf(element);
+            originalArray.splice(indexToRemove, 1);
+        }
+    }
+    return originalArray;
 };
 
 // Do not edit below this line
