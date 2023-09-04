b5123fe1ed58b95e6b70332a1b196d9a7aa3d1cd
Mark Demore <53943861+mdemore2@users.noreply.github.com>
2023-09-04 11:43:57 -0400

    Completed exercise 09
    Parsed input string to remove special characters and ignore changes
    Used built-in array methods to reverse string
    Re-cast vars to string for comparison all in palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (inputString) {
+    inputString = inputString.toLowerCase().replace(/[^A-Za-z0-9]/g, '');
+    charArray = Array.from(inputString);
+    reversed = [...charArray].reverse()
+    return (charArray.toString() === reversed.toString());
 };
 
 // Do not edit below this line
