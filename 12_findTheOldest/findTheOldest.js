26b4014e0dd08882b3aa4572ec45a0cef31d35eb
Mark Demore <53943861+mdemore2@users.noreply.github.com>
2023-09-04 12:37:02 -0400

    Completed exercise 12
    Used Array.reduce() to navigate objects and find oldest person in Array
    Needed to create initial object w/ age of 0 for comparison in findTheOldest.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    // return people.reduce((oldest, person) => person.yearOfBirth < oldest.yearOfBirth ? oldest: person);
+    let currentYear = new Date().getFullYear()
+
+    let oldest = people.reduce((oldest, person) => {
+        if (!person.yearOfDeath){
+            person.age = currentYear - person.yearOfBirth;
+        } else {
+            person.age = person.yearOfDeath - person.yearOfBirth;
+        }
+        return person.age < oldest.age ? oldest : person;
+    }, {age: 0});
+
+    return oldest;
+    
 };
 
 // Do not edit below this line
