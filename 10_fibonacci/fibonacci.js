b8916e077cddfc9f2b0adc55178c4010ecd14f57
Mark Demore <53943861+mdemore2@users.noreply.github.com>
2023-09-04 11:53:27 -0400

    Completed exercise 10
    Added input validation to function in fibonacci.js
    Implemented for loop to dynamically calculate value based on input value in fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    if (index < 0)
+    {
+        return "OOPS";
+    }
 
+    let sequence = [0, 1, 1];
+    for(let i = 0; i <= index; i++) {
+        if (!sequence[i]){
+            sequence[i] = sequence[i-1] + sequence[i-2];
+        }
+    }
+    return sequence[index];
 };
 
 // Do not edit below this line
