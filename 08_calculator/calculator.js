52d5a24b0d6331d29ddeba11b148cecf797a7849
Mark Demore <53943861+mdemore2@users.noreply.github.com>
2023-09-04 11:27:30 -0400

    Completed exercise 08
    Added math functions to calculator.js
    Implemented array.reduce for methods expecting array paramters

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
+const add = function(a, b) {
+	return a + b;
+};
+
+const subtract = function(a, b) {
+	return a - b;
+};
+
+const sum = function(numArray) {
+
+  return numArray.reduce((total, num) => total + num, 0);
 	
 };
 
-const subtract = function() {
-	
+const multiply = function(numArray) {
+  return numArray.reduce((result, num) => result*num, 1);
 };
 
-const sum = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(a) {
+	let result = 1;
+  for (let i = 1; i <= a; i++)
+  {
+    result = result * i;
+  }
+  return result;
 };
 
 // Do not edit below this line
